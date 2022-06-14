b203effecc9b681327a579be993210e731291c7c
Lucas Mirachi <lucas.mirachi@gmail.com>
2022-06-13 23:45:30 -0300

    Update PID.cpp

--- src/PID.cpp
+++ src/PID.cpp
@@ -30,7 +30,7 @@ void PID::UpdateError(double cte) {
    * TODO: Update PID errors based on cte.
    */
   
-  d_error = cte - prev_cte; //differential error
+  d_error = cte - prev_cte; //derivative error
   
   p_error = cte; //proportional error
 
